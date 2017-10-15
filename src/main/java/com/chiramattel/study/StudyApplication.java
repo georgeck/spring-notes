e5ecb6d81bcbec8c33cc421fa149950c9cc4d988
George Chiramattel <george_chiramattel@intuit.com>
2017-10-14 20:37:05 -0700

    Some example of ApplicationContext usage

--- src/main/java/com/chiramattel/study/StudyApplication.java
+++ src/main/java/com/chiramattel/study/StudyApplication.java
@@ -2,11 +2,34 @@ package com.chiramattel.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class StudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyApplication.class, args);
+
+		// An easy (default) way of initializing the `ApplicationContext`.
+		ApplicationContext ctx = SpringApplication.run(StudyApplication.class, args);
+
+		// Alternate way of initializing the ApplicationContext
+		//		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(/*ApplicationConfig.class, Component1.class, Component2.class*/);
+		//		ctx.scan("com.chiramattel.study");
+		//		ctx.refresh();
+
+		// Get all Beans currently registered in context
+		Arrays.asList(ctx.getBeanDefinitionNames()).forEach(System.out::println);
+
+		// Get a specified bean from the context
+		Component1 component1 = ctx.getBean(Component1.class);
+		System.out.println(component1.getDataFromResource());
+
+		// Get the active 'environment' from the context
+		final Environment environment = ctx.getEnvironment();
+		final String[] activeProfiles = environment.getActiveProfiles();
+		Arrays.asList(activeProfiles).forEach(s -> System.out.println("profiles = " + s));
 	}
 }
